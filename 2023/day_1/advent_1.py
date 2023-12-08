8a8b22bcc901e121e1cfea11be8e482372bac5ff
nicoleLab <klaus.kahlfuss@kstp.at>
2023-12-08 23:35:18 +0100

    Refactoring.

--- 2023/day_1/advent_1.py
+++ 2023/day_1/advent_1.py
@@ -1,9 +1,9 @@
-file = "input.txt"
+file: str = "input.txt"
 
 with open(file, "r") as f:
-    input_from_file = [i.strip() for i in f.readlines()]
+    input_from_file: list = [i.strip() for i in f.readlines()]
 
-fixs = {
+fixs: dict = {
     "one": ["oonee", "1"],
     "two": ["ttwoo", "2"],
     "three": ["tthreee", "3"],
@@ -16,14 +16,14 @@ fixs = {
 }
 
 
-def replace_str_from_by_int(x):
+def replace_str_from_list_by_int(x: str) -> str:
     for k, v in fixs.items():
         if k in x:
             x = x.replace(k, fixs[k][0]).replace(k, fixs[k][1])
     return x
 
 
-def get_digit(i):
+def get_first_and_last_digit(i: int) -> int:
     j = i
     while i >= 10:
         i = i / 10
@@ -33,20 +33,22 @@ def get_digit(i):
     return r
 
 
-def double_digit(i):
+def get_double_digit(i: int) -> int:
     r = int(str(i) * 2)
     return r
 
 
-fixed_input = [replace_str_from_by_int(i) for i in input_from_file]
-ints_of_fixed_input = [int("".join(filter(str.isdigit, ws))) for ws in fixed_input]
+fixed_input: list = [replace_str_from_list_by_int(i) for i in input_from_file]
+ints_of_fixed_input: list = [
+    int("".join(filter(str.isdigit, ws))) for ws in fixed_input
+]
 
 for ints in ints_of_fixed_input:
     if ints > 99:
-        replace_ints = get_digit(ints)
+        replace_ints = get_first_and_last_digit(ints)
         ints_of_fixed_input[ints_of_fixed_input.index(ints)] = replace_ints
     if ints < 10:
-        replace_ints = double_digit(ints)
+        replace_ints = get_double_digit(ints)
         ints_of_fixed_input[ints_of_fixed_input.index(ints)] = replace_ints
 
-print(f"RESULT: {sum(ints_of_fixed_input)}")
+print(f"Result: {sum(ints_of_fixed_input)}")
