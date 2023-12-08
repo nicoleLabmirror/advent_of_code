c6e0a86a164505fba8c78398110d2885e4a8b75a
nicoleLab <klaus.kahlfuss@kstp.at>
2023-12-08 18:05:10 +0100

    Look mom I type on my keyboard.

---
+++ 2023/day_1/advent_1.py
@@ -0,0 +1,71 @@
+file = "input.txt"
+
+with open(file, "r") as f:
+    asdf = [i.strip() for i in f.readlines()]
+
+weird_stuff = ["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"]
+
+weird_stuff = [
+    "two1nine",
+    "eightwothree",
+    "abcone2threexyz",
+    "xtwone3four",
+    "4nineeightseven2",
+    "zoneight234",
+    "7pqrstsixteen",
+]
+
+weird_stuff = asdf
+
+fixs = {
+    "one": ["oonee", "1"],
+    "two": ["ttwoo", "2"],
+    "three": ["tthreee", "3"],
+    "four": ["ffourr", "4"],
+    "five": ["ffivee", "5"],
+    "six": ["ssixx", "6"],
+    "seven": ["ssevenn", "7"],
+    "eight": ["eeightt", "8"],
+    "nine": ["nninee", "9"],
+}
+
+
+def fix_inputs(x):
+    for k, v in fixs.items():
+        if k in x:
+            x = x.replace(k, fixs[k][0]).replace(k, fixs[k][1])
+    result = x
+    return result
+
+
+def fix_three_and_longer_digits(i, j):
+    while i >= 10:
+        i = i / 10
+    i = int(i)
+    j = j % 10
+    r = int(str(i) + str(j))
+    return r
+
+
+def fix_one_digit(i):
+    r = int(str(i) * 2)
+    return r
+
+
+print(weird_stuff)
+wtf_stuff = [fix_inputs(i) for i in weird_stuff]
+print(f"Fixed input: {wtf_stuff}")
+
+
+ints_of_weird_stuff = [int("".join(filter(str.isdigit, ws))) for ws in wtf_stuff]
+
+print(ints_of_weird_stuff)
+
+for ints in ints_of_weird_stuff:
+    if ints > 99:
+        replace_ints = fix_three_and_longer_digits(ints, ints)
+        ints_of_weird_stuff[ints_of_weird_stuff.index(ints)] = replace_ints
+    if ints < 10:
+        replace_ints = fix_one_digit(ints)
+        ints_of_weird_stuff[ints_of_weird_stuff.index(ints)] = replace_ints
+print(f"RESULT: {sum(ints_of_weird_stuff)}")
